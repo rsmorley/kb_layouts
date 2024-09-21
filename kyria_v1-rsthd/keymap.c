6dfa5b8c79b30de6710d336cdf9fea44c741cdeb
Scoot <morley.rs@gmail.com>
2024-09-21 16:02:13 -0600

    fix tap dance typos

--- kyria_v1-rsthd/keymap.c
+++ kyria_v1-rsthd/keymap.c
@@ -153,7 +153,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       QK_GESC,        KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       LSFT_T(KC_TAB), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, SC_RSPC,
-      SC_LCPO,        KC_Z,     KC_X,   KC_C,   KC_V,   KC_B,   KC_GRV, KC_NO,   KC_NO, TD(TD_COPY_PASTE),         KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_RCTL, TD(TD_SCREENSHOT),
+      SC_LCPO,        KC_Z,     KC_X,   KC_C,   KC_V,   KC_B,   KC_GRV, KC_NO,   KC_NO, TD(TD_CP_PT),         KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_RCTL, TD(TD_SNST),
               TG(_QWERTY), KC_LCMD , LT(_NUM_NAV, KC_E), LT(_FUNC_MED, KC_BSPC), KC_LALT, KC_RCMD, LT(_FUNC_MED, KC_ENT), LT(_NUM_NAV, KC_SPC),  KC_RALT, KC_MPLY
     ),
 // /*
