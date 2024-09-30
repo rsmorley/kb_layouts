e1304aefc6bb609b83ce6de1d8abefde7b8c6007
Scoot <morley.rs@gmail.com>
2024-09-30 18:12:47 +0000

    add initial corne layout

---
+++ corne-rsthd/rules.mk
@@ -0,0 +1,10 @@
+OLED_ENABLE = yes   # Enables the use of OLED displays
+#OLED_DRIVER = ssd1306
+TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+
+#save space
+LTO_ENABLE = yes
